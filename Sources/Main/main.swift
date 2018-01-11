644d63ca3f1126e594150fa4f4a121a67e723728
sacOO7 <sachinshinde7676@gmail.com>
2018-01-12 02:06:45 +0530

    added code for checking onAuthentication and onSetAuthentication

--- Sources/Main/main.swift
+++ Sources/Main/main.swift
@@ -13,8 +13,18 @@ var onDisconnect = {
         print("Disconnected from server due to ", error?.localizedDescription)
 }
 
-client.setBasicListener(onConnect: onConnect, onConnectError: nil, onDisconnect: onDisconnect)
+var onAuthentication = {
+    (client :ScClient, isAuthenticated : Bool?) in
+    print("Authenticated is ", isAuthenticated)
+}
 
+var onSetAuthentication = {
+    (client : ScClient, token : String?) in
+    print("Token is ", token)
+}
+
+client.setBasicListener(onConnect: onConnect, onConnectError: nil, onDisconnect: onDisconnect)
+client.setAuthenticationListener(onSetAuthentication: onSetAuthentication, onAuthentication: onAuthentication)
 client.connect()
 
 while(true) {
