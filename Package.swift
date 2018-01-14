c3a092b64cd186521249e16c4aaa21bd5454b6d0
sacOO7 <sachinshinde7676@gmail.com>
2018-01-15 00:14:24 +0530

    added library product name and target to package manifest

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "ScClient",
+    products: [
+        .library(
+            name: "ScClient",
+            targets: ["ScClient"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/daltoniam/Starscream.git", .exact("2.1.1")),
